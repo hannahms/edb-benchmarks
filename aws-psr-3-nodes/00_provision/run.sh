d31cbe3e73bedcdc480dbf92bf9b2c0eabc36957
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:23:21 +0000

    aws-psr-3-nodes * save benchmark results to a results directory through the playbook * save terraform infrastructure inside of results directory * upload results directory and terraform directories, excluding any needed data

--- aws-psr-3-nodes/00_provision/run.sh
+++ aws-psr-3-nodes/00_provision/run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -eux
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PLAN_FILENAME="terraform.plan"
 
-edb-terraform ${TERRAFORM_PROJECT_PATH} ${SCRIPT_DIR}/../infrastructure.yml
-cd ${TERRAFORM_PROJECT_PATH}
+# edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
+#   this also includes the edb-terraform version used to generate the files
+edb-terraform generate --project-name ${TERRAFORM_PROJECT_NAME} \
+                       --work-path ${RESULTS_DIRECTORY} \
+                       --infra-file ../infrastructure.yml \
+                       --cloud-service-provider aws
+cd "${TERRAFORM_PROJECT_PATH}"
+
+# .terraform.lock.hcl will be saved here by terraform to lock provider versions and can be reused
 terraform init
-terraform apply -auto-approve
+
+# Save terraform plan for inspection/reuse
+terraform plan -out="$TERRAFORM_PLAN_FILENAME"
+
+# terraform.tfstate/.tfstate.backup will be left around since we use short-term credentials
+terraform apply -auto-approve "$TERRAFORM_PLAN_FILENAME"
