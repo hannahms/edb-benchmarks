e76ac50cc0789af1624d40c0c052eba0e0fc9027
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:20:17 +0000

    aws-dbt2-rds * save benchmark results to a results directory through the playbook * save terraform infrastructure inside of results directory * upload results directory and terraform directories, excluding any needed data

--- aws-dbt2-rds/03_execute/run.sh
+++ aws-dbt2-rds/03_execute/run.sh
@@ -3,6 +3,7 @@
 export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
 # Run the benchmark
 ansible-playbook \
@@ -13,5 +14,9 @@ ansible-playbook \
 	-e "dbt2_duration=${DBT2_DURATION}" \
 	-e "dbt2_warehouse=${DBT2_WAREHOUSE}" \
 	-e "dbt2_connections=${DBT2_CONNECTIONS}" \
-	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+	-e "results_directory=${RESULTS_DIRECTORY}/dbt2-data" \
 	./playbook-dbt2-run.yml
+
+# Copy infrastructure.yml and vars.yml
+cp ../infrastructure.yml "$RESULTS_DIRECTORY/dbt2-data"
+cp ../vars.yml "$RESULTS_DIRECTORY/dbt2-data"
