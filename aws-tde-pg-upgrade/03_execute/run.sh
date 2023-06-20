d94e38afcce20b0a027582db753c6a050a365f2a
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:23:38 +0000

    aws-tde-pg-upgrade * save benchmark results to a results directory through the playbook * save terraform infrastructure inside of results directory * upload results directory and terraform directories, excluding any needed data

--- aws-tde-pg-upgrade/03_execute/run.sh
+++ aws-tde-pg-upgrade/03_execute/run.sh
@@ -5,24 +5,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
-# Run the pg_upgrade benchmark with TDE
+# Run the pg_upgrade benchmark with TDE and without TDE
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
 	-i ${SCRIPT_DIR}/../inventory.yml \
 	-e "@${SCRIPT_DIR}/../vars.yml" \
-	-e "use_tde=1" \
+	-e "results_directory=${RESULTS_DIRECTORY}/report-data" \
 	${SCRIPT_DIR}/playbook-pg-upgrade-timing.yml
 
-# Run the pg_upgrade benchmark without TDE
-ansible-playbook \
-	-u ${SSH_USER} \
-	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
-	-e "use_tde=0" \
-	${SCRIPT_DIR}/playbook-pg-upgrade-timing.yml
-
-# Generate charts
-python3 ${SCRIPT_DIR}/post-processing.py
+# Copy infrastructure.yml and vars.yml
+cp "../infrastructure.yml" "$RESULTS_DIRECTORY"
+cp "../vars.yml" "$RESULTS_DIRECTORY"
--- aws-tde-pg-upgrade/04_push_results/run.sh
+++ aws-tde-pg-upgrade/04_push_results/run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -eux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
 # Push pgbench files to the S3 bucket
-
-# Copy collected data and generated data & charts
-cp -r ${SCRIPT_DIR}/../03_execute/benchmark_data ${SCRIPT_DIR}/report-data
-
+cd "$RESULTS_DIRECTORY"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
-aws s3 cp ${SCRIPT_DIR}/../infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/infrastructure.yml
-aws s3 cp ${SCRIPT_DIR}/../vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/vars.yml
-aws s3 cp ${SCRIPT_DIR}/report-data/ s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/report-data --recursive
+# Upload infrastructure terraform project files.
+# Exclude the terraform state, any ssh keys and .terraform cache as the lock hcl file should be available.
+aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" \
+            --exclude *tfstate* \
+            --exclude *ssh* \
+            --exclude .terraform/** \
+            --recursive
+# Upload benchmark data
+aws s3 cp ${RESULTS_DIRECTORY}/infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/infrastructure.yml
+aws s3 cp ${RESULTS_DIRECTORY}/vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/vars.yml
+aws s3 cp ${RESULTS_DIRECTORY}/report-data/ s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/report-data --recursive
