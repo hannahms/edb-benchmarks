f81834db4023f0743f05ab9b51e98bd09f71e027
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:21:29 +0000

    aws-hammerdb-tproc-c-biganimal * save benchmark results to a results directory through the playbook * save terraform infrastructure inside of results directory * upload results directory and terraform directories, excluding any needed data

--- aws-hammerdb-tproc-c-biganimal/03_execute/run.sh
+++ aws-hammerdb-tproc-c-biganimal/03_execute/run.sh
@@ -4,6 +4,7 @@ SSH_USER=rocky
 export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
 # Run the benchmark
 ansible-playbook \
@@ -13,4 +14,9 @@ ansible-playbook \
 	-i ../inventory.yml \
 	-e "@../vars.yml" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+	-e "results_directory=${RESULTS_DIRECTORY}/tprocc-data" \
 	./playbook-tprocc-run.yml
+
+# Copy infrastructure.yml and vars.yml
+cp "../infrastructure.yml" "$RESULTS_DIRECTORY/tprocc-data"
+cp "../vars.yml" "$RESULTS_DIRECTORY/tprocc-data"
