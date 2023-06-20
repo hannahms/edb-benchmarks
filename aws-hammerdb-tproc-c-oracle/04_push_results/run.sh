c0f842afa62ef24347b5e6335c758c5cbc84a070
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:21:57 +0000

    aws-hammerdb-tproc-c-oracle * save benchmark results to a results directory through the playbook * save terraform infrastructure inside of results directory * upload results directory and terraform directories, excluding any needed data

--- aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
+++ aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -eux
 # Push HammerDB files to the S3 bucket
-
-# Extract the archive containing the report and data
-mkdir -p tprocc-data
-cp -pr ../03_execute/hammerdb.log tprocc-data/.
-cp -pr ../03_execute/hdbxtprofile.log tprocc-data/.
-# Copy infrastructure.yml and vars.yml
-cp ../infrastructure.yml tprocc-data/.
-cp ../vars.yml tprocc-data/.
-cd tprocc-data
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+cd "${RESULTS_DIRECTORY}/tprocc-data"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
+# Upload infrastructure terraform project files.
+# Exclude the terraform state, any ssh keys and .terraform cache as the lock hcl file should be available.
+aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" \
+            --exclude *tfstate* \
+            --exclude *ssh* \
+            --exclude .terraform/** \
+            --recursive
+# Upload benchmark data
 aws s3 cp ./ "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/" --recursive
