83510e74e45992d3fa7a6a2ba261feecd92724aa
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-04 12:03:58 +0000

    aws-dbt2-biganimal * Provision BigAnimal infrastructure with terraform instead of the CLI   * infrastructure.yml file updated with biganimal   * inventory.yml.tftpl terraform template files, replaces jinja file inventory.yml.j2, so that the file generation may be handled by the edb-terraform modules.   * BA_PROJECT_ID environment variable required so it may be set at runtime within infrastructure.yml   * BA_BEARER_TOKEN environment variable auth token (currently lasts 24 hours) required to use the BigAnimal Provider

--- aws-dbt2-biganimal/00_provision/run.sh
+++ aws-dbt2-biganimal/00_provision/run.sh
@@ -6,12 +6,17 @@ RUNDIR=$(dirname "$RUNDIR")
 TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
 
+# set biganimal project id in infrastructure.yml
+sed -i "s/<PROJECT_ID>/${BA_PROJECT_ID}/g" ${RUNDIR}/../infrastructure.yml
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
 edb-terraform generate --project-name ${TERRAFORM_PROJECT_NAME} \
                        --work-path ${RESULTS_DIRECTORY} \
-                       --infra-file ../infrastructure.yml \
+                       --infra-file ${RUNDIR}/../infrastructure.yml \
+                       --user-templates ${RUNDIR}/templates/inventory.yml.tftpl \
                        --cloud-service-provider aws
+# reset biganimal project id for reuse and edb-terraform creates infrastructure.yml.bak within the project directory
+sed -i "s/${BA_PROJECT_ID}/<PROJECT_ID>/g" ${RUNDIR}/../infrastructure.yml
 cd ${TERRAFORM_PROJECT_PATH}
 
 # .terraform.lock.hcl will be saved here by terraform to lock provider versions and can be reused
@@ -22,73 +27,3 @@ terraform plan -out="$TERRAFORM_PLAN_FILENAME"
 
 # terraform.tfstate/.tfstate.backup will be left around since we use short-term credentials
 terraform apply -auto-approve "$TERRAFORM_PLAN_FILENAME"
-
-# We need the absolute path of $TERRAFORM_PROJECT_PATH in this script.
-TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
-TERRAFORM_PROJECT_PATH=$(readlink -f "${TERRAFORM_PROJECT_PATH}")
-BIGANIMALINFRAFILE="${TERRAFORM_PROJECT_PATH}/ba-infrastructure.yml"
-
-UNIQUEHASH=$(xxd -l 4 -c 4 -p < /dev/random)
-
-cat >> "${BIGANIMALINFRAFILE}" << EOF
----
-clusterArchitecture: ha
-haStandbyReplicas: 1
-provider: aws
-clusterName: dbt2-${UNIQUEHASH}
-password: 1234567890zyx
-postgresType: postgres
-postgresVersion: "14"
-region: us-east-1
-instanceType: aws:r5.8xlarge
-volumeType: io2
-volumeProperties: io2
-volumePropertySize: "4096 Gi"
-volumePropertyIOPS: 64000
-networking: public
-EOF
-
-# Note that biganimal cli outputs create-cluster messages to stderr.
-OUTPUT=$(biganimal create-cluster --cluster-config-file "${BIGANIMALINFRAFILE}" -y 2>&1)
-PATTERN='"(.+)"'
-[[ $OUTPUT =~ $PATTERN ]]
-BAID="${BASH_REMATCH[1]}"
-
-BIGANIMALIDFILE="${TERRAFORM_PROJECT_PATH}/biganimal-id"
-
-echo "$BAID" > "${BIGANIMALIDFILE}"
-
-# TODO: We're only checking if the cluster is healthy.  May need more logic to
-# handle other situations or to reply on buildbot to time out after a certain
-# amount of time.
-RC=0
-while [ $RC -ne 1 ]; do
-	RC=$(biganimal show-clusters --id "$BAID" | grep "\<$BAID\>" | grep -c "Healthy" || exit 0)
-	biganimal show-clusters --id "$BAID"
-	sleep 2
-done
-
-#
-# Manually append servers.yml file with BigAnimal details.
-#
-
-TOOMUCHINFO=$(biganimal show-cluster-connection --id "$BAID" | grep read-write)
-
-PATTERN='@(.+):'
-[[ $TOOMUCHINFO =~ $PATTERN ]]
-BAADDRESS="${BASH_REMATCH[1]}"
-
-PATTERN='postgresql://(.+)@'
-[[ $TOOMUCHINFO =~ $PATTERN ]]
-BAPGUSER="${BASH_REMATCH[1]}"
-
-cat >> "${TERRAFORM_PROJECT_PATH}/servers.yml" << EOF
-  databases:
-    dbt2:
-      region: us-east-1
-      username: "$BAPGUSER"
-      password: "1234567890zyx"
-      address: $BAADDRESS
-      port: 5432
-      dbname: "dbt2"
-EOF
--- aws-dbt2-biganimal/05_unprovision/run.sh
+++ aws-dbt2-biganimal/05_unprovision/run.sh
@@ -6,9 +6,3 @@ TERRAFORM_PROJECT_PATH=$(readlink -f "${TERRAFORM_PROJECT_PATH}")
 
 cd "${TERRAFORM_PROJECT_PATH}"
 terraform destroy -auto-approve
-
-BIGANIMALIDFILE="${TERRAFORM_PROJECT_PATH}/biganimal-id"
-BAID=$(<"$BIGANIMALIDFILE")
-
-biganimal delete-cluster --id "$BAID" -y
-# TODO: how to make script wait until bignimal finishes destroying cluster?
