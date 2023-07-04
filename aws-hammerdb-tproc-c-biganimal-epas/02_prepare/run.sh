22000693d02ff2b34b866f2103047a23d11ec4b1
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-04 11:37:21 +0000

    aws-hammerdb-tproc-c-biganimal-epas * Provision BigAnimal infrastructure with terraform instead of the CLI   * inventory.yml file now generated with terraform     * includes ssh file location and ssh user   * reference values from servers.yml   * async re-added to steps to allow for execution from local environments

--- aws-hammerdb-tproc-c-biganimal-epas/02_prepare/run.sh
+++ aws-hammerdb-tproc-c-biganimal-epas/02_prepare/run.sh
@@ -7,9 +7,7 @@ export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
 ansible-playbook \
-	-u ${SSH_USER} \
-	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ../inventory.yml \
+	-i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
 	-e "@../vars.yml" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
 	./playbook-tprocc-build-db.yml
