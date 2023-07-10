c446d7e3d0960bfaf79ea81d3528357898ccddb4
Mark Wong <mark.wong@enterprisedb.com>
2023-07-10 08:22:12 -0700

    aws-pgbench-postgresql: rewrite post-processing code
    Use something more stable and portable with gnuplot instead of Python
    and Pandas.

--- aws-pgbench-postgresql/04_execute/files/exec-pgbench-rampup.sh
+++ aws-pgbench-postgresql/04_execute/files/exec-pgbench-rampup.sh
@@ -13,8 +13,10 @@ PGSQL_BIN_PATH=/usr/local/pgsql-${PG_MAX_VERSION}/bin
 # Let's make read-write the default mode if no argument is given
 PGBENCH_MODE=${1:-read-write}
 
-echo "clients,${PG_VERSION}" > /tmp/pgbench-tps-${PG_VERSION}.csv
-echo "0,0" >> /tmp/pgbench-tps-${PG_VERSION}.csv
+# Make "space" separated values, for gnuplot.
+
+echo "clients ${PG_VERSION}" > /tmp/pgbench-tps-${PG_VERSION}.csv
+echo "0 0" >> /tmp/pgbench-tps-${PG_VERSION}.csv
 
 for ((c=${CLIENT_START}; c<${CLIENT_END}; c=c+${CLIENT_STEP}))
 do
@@ -26,6 +28,6 @@ do
        # Execute pgbench in read-only mode
        tps=$(${PGSQL_BIN_PATH}/pgbench -T ${MY_BENCHMARK_DURATION} -S -c ${c} -j 32 pgbench 2> /dev/null | grep "tps = " | grep -v "including" | sed -E "s/tps = ([0-9\.]+).*/\1/")
     fi
-    echo "${c},${tps}" >> /tmp/pgbench-tps-${PG_VERSION}.csv
+    echo "${c} ${tps}" >> /tmp/pgbench-tps-${PG_VERSION}.csv
     sleep 5
 done
