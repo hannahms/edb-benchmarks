e9df5aa6c0a4d91892af896e4ba891bdeab51b9b
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-11 22:56:04 +0000

    aws-hammerdb-tproc-c-oracle - copy terraform files to results directory so a recursive upload can be done

--- aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
+++ aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -eux
 # Push HammerDB files to the S3 bucket
-TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
-cd "${RESULTS_DIRECTORY}/tprocc-data"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
-# Upload infrastructure terraform project files.
-# Exclude the terraform state, any ssh keys and .terraform cache as the lock hcl file should be available.
-aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" \
-            --exclude *tfstate* \
-            --exclude *ssh* \
-            --exclude .terraform/** \
-            --recursive
 # Upload benchmark data
-aws s3 cp ./ "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/" --recursive
+aws s3 cp "$RESULTS_DIRECTORY/" s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/ --recursive
