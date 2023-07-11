89ec3aa11de5cbc339ea973c3847d1e6391bfe5e
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-11 22:56:33 +0000

    aws-pgbench-postgresql - copy terraform files to results directory so a recursive upload can be done

--- aws-pgbench-postgresql/01_provision/run.sh
+++ aws-pgbench-postgresql/01_provision/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -eux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
 
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
 edb-terraform generate --project-name "${TERRAFORM_PROJECT_NAME}" \
-                       --work-path "${RESULTS_DIRECTORY}" \
+                       --work-path ../ \
                        --infra-file "${SCRIPT_DIR}/../infrastructure.yml" \
                        --cloud-service-provider aws
 cd "${TERRAFORM_PROJECT_PATH}"
@@ -20,3 +21,13 @@ terraform plan -out="$TERRAFORM_PLAN_FILENAME"
 
 # terraform.tfstate/.tfstate.backup will be left around since we use short-term credentials
 terraform apply -auto-approve "$TERRAFORM_PLAN_FILENAME"
+
+# copy files into results directory
+mkdir -p "${RESULTS_DIRECTORY}" && \ 
+rsync --archive \
+      --exclude='*tfstate*' \ # might contain secrets
+      --exclude='*ssh*' \ # short term keys currently used but can be long term
+      --exclude='.terraform/' \ # created at run-time and controlled by terraform
+      --recursive \
+      ./ \
+      "$RESULTS_DIRECTORY/terraform"
