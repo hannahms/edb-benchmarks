23ed4873017c3ed4cae2b6d3cf974337880edee9
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-11 22:55:01 +0000

    aws-edb-postgres-tuner-dbt2 - copy terraform files to results directory so a recursive upload can be done

--- aws-edb-postgres-tuner-dbt2/05_unprovision/run.sh
+++ aws-edb-postgres-tuner-dbt2/05_unprovision/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
-TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PROJECT_PATH="../terraform"
 cd "${TERRAFORM_PROJECT_PATH}"
 terraform destroy -auto-approve
