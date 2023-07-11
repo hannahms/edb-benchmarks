c828f99fcf8b7350a14a00c1cb6727a3383a352d
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-11 22:56:16 +0000

    aws-lsr-3-nodes - copy terraform files to results directory so a recursive upload can be done

--- aws-lsr-3-nodes/04_push_results/run.sh
+++ aws-lsr-3-nodes/04_push_results/run.sh
@@ -1,20 +1,6 @@
 #!/bin/bash -eux
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 # Push pgbench files to the S3 bucket
-TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
-cd "$RESULTS_DIRECTORY"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
-# Upload infrastructure terraform project files.
-# Exclude the terraform state, any ssh keys and .terraform cache as the lock hcl file should be available.
-aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" \
-            --exclude *tfstate* \
-            --exclude *ssh* \
-            --exclude .terraform/** \
-            --recursive
 # Upload benchmark data
-aws s3 cp ${RESULTS_DIRECTORY}/infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/infrastructure.yml
-aws s3 cp ${RESULTS_DIRECTORY}/vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/vars.yml
-aws s3 cp ${RESULTS_DIRECTORY}/report-data/ s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/report-data --recursive
+aws s3 cp "$RESULTS_DIRECTORY/" s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/ --recursive
