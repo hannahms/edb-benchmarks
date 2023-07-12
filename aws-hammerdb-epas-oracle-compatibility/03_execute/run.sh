6ac6d7d84b8694afafff4fdd4e0628c6f6dab691
Mark Wong <mark.wong@enterprisedb.com>
2023-07-12 15:03:48 -0700

    aws-hammerdb-epas-oracle-compatibility: save files at top level
    There's no need to maintain a tprocc-data subdirectory.  Save files at
    the top.

--- aws-hammerdb-epas-oracle-compatibility/03_execute/run.sh
+++ aws-hammerdb-epas-oracle-compatibility/03_execute/run.sh
@@ -10,9 +10,9 @@ ansible-playbook \
 	-i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
 	-e "@../vars.yml" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
-	-e "results_directory=${RESULTS_DIRECTORY}/tprocc-data" \
+	-e "results_directory=${RESULTS_DIRECTORY}" \
 	./playbook-tprocc-run.yml
 
 # Copy infrastructure.yml and vars.yml
-cp "../infrastructure.yml" "$RESULTS_DIRECTORY/tprocc-data"
-cp "../vars.yml" "$RESULTS_DIRECTORY/tprocc-data"
+cp "../infrastructure.yml" "${RESULTS_DIRECTORY}"
+cp "../vars.yml" "${RESULTS_DIRECTORY}"
