b84e3e36c41b631557216f4b873ce168ea66cf69
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:26:56 +0000

    aws-hammerdb-tproc-c-biganimal-epas - get script directory paths and update any needed path variables

--- aws-hammerdb-tproc-c-biganimal-epas/00_provision/run.sh
+++ aws-hammerdb-tproc-c-biganimal-epas/00_provision/run.sh
@@ -1,23 +1,37 @@
 #!/bin/bash -eux
 
-RUNDIR=$(readlink -f "${BASH_SOURCE[0]}")
-RUNDIR=$(dirname "$RUNDIR")
-
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 TERRAFORM_PROJECT_NAME="terraform"
-TERRAFORM_PROJECT_PATH="../${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
-# set biganimal project id in infrastructure.yml
-sed -i "s/<PROJECT_ID>/${BA_PROJECT_ID}/g" ${RUNDIR}/../infrastructure.yml
+ansible-playbook "$RUNDIR/generate.yml" \
+      -e "template_file=$RUNDIR/../templates/infrastructure.yml.j2" \
+      -e "dest_file=$RUNDIR/../infrastructure.yml" \
+      -e "@$RUNDIR/../vars.yml" \
+      -e "region=us-east-1" \
+      -e "zone0=us-east-1b" \
+      -e "instance_type=c5.9xlarge" \
+      -e "pg_instance_type=r5.4xlarge" \
+      -e "pg_storage_type=io2" \
+      -e "pg_storage_properties=io2" \
+      -e "pg_storage_size=300" \
+      -e "pg_storage_iops=15000" \
+      -e "pg_engine=epas" \
+      -e "pg_version=14" \
+      -e "pg_password=1234567890zyx" \
+      -e "ba_project_id=$BA_PROJECT_ID"
+
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
-edb-terraform generate --project-name ${TERRAFORM_PROJECT_NAME} \
-                       --work-path ../ \
-                       --infra-file ${RUNDIR}/../infrastructure.yml \
-                       --user-templates ${RUNDIR}/templates/inventory.yml.tftpl \
+edb-terraform generate --project-name "${TERRAFORM_PROJECT_NAME}" \
+                       --work-path "${SOURCEDIR}/../" \
+                       --infra-file "${SOURCEDIR}../infrastructure.yml" \
+                       --user-templates "${SOURCEDIR}/templates/inventory.yml.tftpl" \
                        --cloud-service-provider aws
 # reset biganimal project id for reuse and edb-terraform creates infrastructure.yml.bak within the project directory
-sed -i "s/${BA_PROJECT_ID}/<PROJECT_ID>/g" ${RUNDIR}/../infrastructure.yml
+sed -i "s/${BA_PROJECT_ID}/<PROJECT_ID>/g" "${SOURCEDIR}/../infrastructure.yml"
 cd "${TERRAFORM_PROJECT_PATH}"
 
 # .terraform.lock.hcl will be saved here by terraform to lock provider versions and can be reused
@@ -39,5 +53,5 @@ rsync --archive \
       --exclude="*ssh*" \
       --exclude=".terraform/" \
       --recursive \
-      ./ \
-      "$RESULTS_DIRECTORY/terraform"
+      "${TERRAFORM_PROJECT_PATH}" \
+      "${RESULTS_DIRECTORY}/"
