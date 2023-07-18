47b01eb185ed07a64566470ea68e368bccccb47b
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:29:47 +0000

    aws-tpcc-tde-medium-amd - get script directory paths and update any needed path variables

--- aws-tpcc-tde-medium-amd/04_push_results/run.sh
+++ aws-tpcc-tde-medium-amd/04_push_results/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # Upload benchmark data
-aws s3 cp "$RESULTS_DIRECTORY/" s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/ --recursive
+aws s3 cp "$RESULTS_DIRECTORY/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/" --recursive
