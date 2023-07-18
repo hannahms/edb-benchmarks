ea63d17f3f33dc3d1f7fcc5664bd9f2450a51a13
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:26:37 +0000

    aws-hammerdb-tproc-c-biganimal - get script directory paths and update any needed path variables

--- aws-hammerdb-tproc-c-biganimal/01_deploy/run.sh
+++ aws-hammerdb-tproc-c-biganimal/01_deploy/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -eux
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 
 export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-TERRAFORM_PROJECT_PATH="../terraform"
 
 ansible-playbook \
 	-i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
