bde15dd5b4c3563073726a86a62145de3858c5f9
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 12:13:39 +0000

    aws-edb-postgres-tuner-dbt2 - generate the infrastructure file with jinja

--- aws-edb-postgres-tuner-dbt2/00_provision/run.sh
+++ aws-edb-postgres-tuner-dbt2/00_provision/run.sh
@@ -6,11 +6,24 @@ TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
 RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
+ansible-playbook $RUNDIR/generate.yml \
+      -e "template_file=$RUNDIR/../templates/infrastructure.yml.j2" \
+      -e "dest_file=$RUNDIR/../infrastructure.yml" \
+      -e "@$RUNDIR/../vars.yml" \
+      -e "region=us-east-1" \
+      -e "zone0=us-east-1b" \
+      -e "zone1=us-east-1c" \
+      -e "instance_type=m5.2xlarge" \
+      -e "pg_instance_type=c5.2xlarge" \
+      -e "pg_storage_type=io2" \
+      -e "pg_storage_size=1500" \
+      -e "pg_storage_iops=32000"
+
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
 edb-terraform generate --project-name "${TERRAFORM_PROJECT_NAME}" \
                        --work-path "${SOURCEDIR}/../" \
-                       --infra-file "${SOURCEDIR}../infrastructure.yml" \
+                       --infra-file "${SOURCEDIR}/../infrastructure.yml" \
                        --cloud-service-provider aws
 cd "${TERRAFORM_PROJECT_PATH}"
 
