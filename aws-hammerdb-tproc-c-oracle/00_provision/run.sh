1b73e3d764616ae017817f109fc846d5cc1f6bdc
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 12:47:06 +0000

    aws-hammerdb-tproc-c-oracle - generate the infrastructure file with jinja

--- aws-hammerdb-tproc-c-oracle/00_provision/run.sh
+++ aws-hammerdb-tproc-c-oracle/00_provision/run.sh
@@ -6,11 +6,28 @@ TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
 RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
+ansible-playbook $RUNDIR/generate.yml \
+      -e "template_file=$RUNDIR/../templates/infrastructure.yml.j2" \
+      -e "dest_file=$RUNDIR/../infrastructure.yml" \
+      -e "@$RUNDIR/../vars.yml" \
+      -e "region=us-east-1" \
+      -e "zone0=us-east-1b" \
+      -e "zone1=us-east-1c" \
+      -e "instance_type=c5.9xlarge" \
+      -e "db_instance_type=db.r5.8xlarge" \
+      -e "db_storage_size=300" \
+      -e "db_storage_type=io1" \
+      -e "db_storage_iops=15000" \
+      -e "db_engine=oracle-ee"
+      -e "db_version=19" \
+      -e "db_superuser=admin" \
+      -e "db_password=12Password!"
+
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
 edb-terraform generate --project-name "${TERRAFORM_PROJECT_NAME}" \
                        --work-path "${SOURCEDIR}/../" \
-                       --infra-file "${SOURCEDIR}../infrastructure.yml" \
+                       --infra-file "${SOURCEDIR}/../infrastructure.yml" \
                        --cloud-service-provider aws
 cd "${TERRAFORM_PROJECT_PATH}"
 
