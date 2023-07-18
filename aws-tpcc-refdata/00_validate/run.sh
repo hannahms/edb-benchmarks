3e82c93134da9c0ba082d3caed99ae9fc9463521
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:29:02 +0000

    aws-tpcc-refdata - get script directory paths and update any needed path variables

--- aws-tpcc-refdata/00_validate/run.sh
+++ aws-tpcc-refdata/00_validate/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eux
 
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PLAN_FILENAME="terraform.plan"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
 if (( $(echo "$tpcc_warehouse < 15" | bc -l) )); then
    echo "tpcc_warehouse: $tpcc_warehouse";
    echo "HammerDB Warehouses, cannot be lower than 15!"
--- aws-tpcc-refdata/04_execute/run.sh
+++ aws-tpcc-refdata/04_execute/run.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -eux
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
 export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-TERRAFORM_PROJECT_PATH="../terraform"
 
 # Run the benchmark with refdata enabled and disabled
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-i ${SOURCEDIR}/../inventory.yml \
+	-e "@${SOURCEDIR}/../vars.yml" \
 	-e "tpcc_duration=${TPCC_DURATION}" \
 	-e "tpcc_rampup=${TPCC_RAMPUP}" \
 	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
 	-e "tpcc_vusers=${TPCC_VUSERS}" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
 	-e "results_directory=${RESULTS_DIRECTORY}/report-data" \
-	${SCRIPT_DIR}/playbook-tpcc-run.yml
+	${SOURCEDIR}/playbook-tpcc-run.yml
 
 # Copy infrastructure.yml and vars.yml
 cp "../infrastructure.yml" "$RESULTS_DIRECTORY"
