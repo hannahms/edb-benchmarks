81cc401f609180bba44ec8586b2728891e119d36
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:22:45 +0000

    aws-dbt2-biganimal - get script directory paths and update any needed path variables

--- aws-dbt2-biganimal/03_execute/run.sh
+++ aws-dbt2-biganimal/03_execute/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -eux
 
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
 export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-export TERRAFORM_PROJECT_PATH="../terraform"
-
 
 # Run the benchmark
 ansible-playbook \
