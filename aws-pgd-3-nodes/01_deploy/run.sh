49fc3be2550f62913f2736369df09f780f98a376
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:28:07 +0000

    aws-pgd-3-nodes - get script directory paths and update any needed path variables

--- aws-pgd-3-nodes/01_deploy/run.sh
+++ aws-pgd-3-nodes/01_deploy/run.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -eux
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 
 export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-TERRAFORM_PROJECT_PATH="../terraform"
 
 # Create the TPA directory
-mkdir -p ${SCRIPT_DIR}/tpa
+mkdir -p ${SOURCEDIR}/tpa
 
-python3 ${SCRIPT_DIR}/build-inventory.py ${TERRAFORM_PROJECT_PATH}
-mv ${SCRIPT_DIR}/inventory.yml ${SCRIPT_DIR}/../.
+python3 ${SOURCEDIR}/build-inventory.py ${TERRAFORM_PROJECT_PATH}
+mv ${SOURCEDIR}/inventory.yml ${SOURCEDIR}/../.
 
 # TPA configuration
-TPA_DIR=${SCRIPT_DIR}/tpa
-mv ${SCRIPT_DIR}/config.yml ${TPA_DIR}/.
-mv ${SCRIPT_DIR}/edb-repo-creds.txt ${TPA_DIR}/.
+TPA_DIR=${SOURCEDIR}/tpa
+mv ${SOURCEDIR}/config.yml ${TPA_DIR}/.
+mv ${SOURCEDIR}/edb-repo-creds.txt ${TPA_DIR}/.
 chmod 0600 ${TPA_DIR}/edb-repo-creds.txt
 export EDB_REPO_CREDENTIALS_FILE=${TPA_DIR}/edb-repo-creds.txt
 
@@ -27,9 +29,9 @@ ${TPA_BIN_DIR}/tpaexec provision ${TPA_DIR}
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
-	${SCRIPT_DIR}/playbook-setup-fs.yml
+	-i ${SOURCEDIR}/../inventory.yml \
+	-e "@${SOURCEDIR}/../vars.yml" \
+	${SOURCEDIR}/playbook-setup-fs.yml
 
 # TPA deployment
 ${TPA_BIN_DIR}/tpaexec deploy ${TPA_DIR}
@@ -37,13 +39,13 @@ ${TPA_BIN_DIR}/tpaexec deploy ${TPA_DIR}
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
-	${SCRIPT_DIR}/playbook-deploy.yml
+	-i ${SOURCEDIR}/../inventory.yml \
+	-e "@${SOURCEDIR}/../vars.yml" \
+	${SOURCEDIR}/playbook-deploy.yml
 
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
-	${SCRIPT_DIR}/playbook-hammerdb-setup.yml
+	-i ${SOURCEDIR}/../inventory.yml \
+	-e "@${SOURCEDIR}/../vars.yml" \
+	${SOURCEDIR}/playbook-hammerdb-setup.yml
