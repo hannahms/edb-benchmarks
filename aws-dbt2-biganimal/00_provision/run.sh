403d3bd9d13a1b3641a45262a0f0c6bd4970d561
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 03:38:02 +0000

    aws-dbt2-biganimal - generate the infrastructure file with jinja

--- aws-dbt2-biganimal/00_provision/run.sh
+++ aws-dbt2-biganimal/00_provision/run.sh
@@ -6,17 +6,30 @@ TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
 RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
-# set biganimal project id in infrastructure.yml
-sed -i "s/<PROJECT_ID>/${BA_PROJECT_ID}/g" ${RUNDIR}/../infrastructure.yml
+ansible-playbook $RUNDIR/generate.yml \
+      -e "template_file=$RUNDIR/templates/infrastructure.yml.j2" \
+      -e "dest_file=$RUNDIR/../infrastructure.yml" \
+      -e "@$RUNDIR/../vars.yml" \
+      -e "region=us-east-1" \
+      -e "zone0=us-east-1b" \
+      -e "instance_type=c5.9xlarge" \
+      -e "pg_instance_type=r5.8xlarge" \
+      -e "pg_storage_type=io2" \
+      -e "pg_storage_properties=io2" \
+      -e "pg_storage_size=4096" \
+      -e "pg_storage_iops=64000" \
+      -e "pg_engine=postgres" \
+      -e "pg_version=14" \
+      -e "pg_password=1234567890zyx" \
+      -e "ba_project_id=$BA_PROJECT_ID"
+
 # edb-terraform saves a backup of infrastructure.yml in <project-name>/infrastructure.yml.bak
 #   this also includes the edb-terraform version used to generate the files
 edb-terraform generate --project-name "${TERRAFORM_PROJECT_NAME}" \
                        --work-path "${SOURCEDIR}/../" \
-                       --infra-file "${SOURCEDIR}../infrastructure.yml" \
+                       --infra-file "${SOURCEDIR}/../infrastructure.yml" \
                        --user-templates "${SOURCEDIR}/templates/inventory.yml.tftpl" \
                        --cloud-service-provider aws
-# reset biganimal project id for reuse and edb-terraform creates infrastructure.yml.bak within the project directory
-sed -i "s/${BA_PROJECT_ID}/<PROJECT_ID>/g" "${SOURCEDIR}/../infrastructure.yml"
 cd "${TERRAFORM_PROJECT_PATH}"
 
 # .terraform.lock.hcl will be saved here by terraform to lock provider versions and can be reused
