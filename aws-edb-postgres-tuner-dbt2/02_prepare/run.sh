1d2e5d07ea4038c10aa7368b3c7a617f65418a8e
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-18 01:23:32 +0000

    aws-edb-postgres-tuner-dbt2 - get script directory paths and update any needed path variables

--- aws-edb-postgres-tuner-dbt2/02_prepare/run.sh
+++ aws-edb-postgres-tuner-dbt2/02_prepare/run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -eux
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 
 export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-TERRAFORM_PROJECT_PATH="../terraform"
 
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key "${TERRAFORM_PROJECT_PATH}/ssh-id_rsa" \
-	-i "${SCRIPT_DIR}/../inventory.yml" \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-i "${SOURCEDIR}/../inventory.yml" \
+	-e "@${SOURCEDIR}/../vars.yml" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
 	-e "dbt2_warehouse=${DBT2_WAREHOUSE}" \
-	"${SCRIPT_DIR}/playbook-dbt2-build-db.yml"
+	"${SOURCEDIR}/playbook-dbt2-build-db.yml"
--- aws-edb-postgres-tuner-dbt2/03_execute/run.sh
+++ aws-edb-postgres-tuner-dbt2/03_execute/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -eux
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
 export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
-TERRAFORM_PROJECT_PATH="../terraform"
 
 # Run the benchmark
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key "${TERRAFORM_PROJECT_PATH}/ssh-id_rsa" \
-	-i "${SCRIPT_DIR}/../inventory.yml" \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-i "${SOURCEDIR}/../inventory.yml" \
+	-e "@${SOURCEDIR}/../vars.yml" \
 	-e "dbt2_duration=${DBT2_DURATION}" \
 	-e "dbt2_warehouse=${DBT2_WAREHOUSE}" \
 	-e "dbt2_connections=${DBT2_CONNECTIONS}" \
@@ -23,5 +24,5 @@ ansible-playbook \
 	./playbook-dbt2-run.yml
 
 # Copy infrastructure.yml and vars.yml
-cp "${SCRIPT_DIR}/../infrastructure.yml" "${SCRIPT_DIR}/dbt2-data/"
-cp "${SCRIPT_DIR}/../vars.yml" "${RESULTS_DIRECTORY}/dbt2-data/"
+cp "${SOURCEDIR}/../infrastructure.yml" "${SOURCEDIR}/dbt2-data/"
+cp "${SOURCEDIR}/../vars.yml" "${RESULTS_DIRECTORY}/dbt2-data/"
