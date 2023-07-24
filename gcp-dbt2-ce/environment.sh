9271e9403e3f8e80a4fcf6d681ddfd5dccd26bcc
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-24 23:45:01 +0000

    gcp-dbt2-ce - generate environment variables yaml file from environment.sh and move infrastructure to a template

---
+++ gcp-dbt2-ce/environment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+# Generic
+export BUCKET_NAME=${BUCKET_NAME:=ebac-reports}
+export BENCHMARK_NAME=${BENCHMARK_NAME:=AWS_DBT2_AURORA}
+
+# Ansible
+export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
+
+# TERRAFORM VARIABLES
+export REGION="${REGION:=us-west1}"
+export ZONE0="${ZONE0:=us-west1-b}"
+export INSTANCE_TYPE="${INSTANCE_TYPE:=c2-standard-30}"
+
+# Postgres Settings
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c2-standard-30}"
+export PG_STORAGE_TYPE="${PG_STORAGE_TYPE:=ssd}"
+export PG_STORAGE_SIZE="${PG_STORAGE_SIZE:=4096}"
+export PG_STORAGE_IOPS="${PG_STORAGE_IOPS:=1000}"
+
+# DBT2
+export DBT2_CONNECTIONS="${DBT2_CONNECTIONS:=72}"
+export DBT2_DURATION="${DBT2_DURATION=:3600}"
+export DBT2_WAREHOUSE="${DBT2_WAREHOUSE=:10000}"
