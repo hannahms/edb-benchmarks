c2fd7bc26b279229244f62331cb1c58862b4863d
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 16:53:17 +0000

    aws-lsr-3-nodes - move infrastructure file to a jinja template and create final file during provision step

--- aws-lsr-3-nodes/environment.sh
+++ aws-lsr-3-nodes/environment.sh
@@ -8,6 +8,14 @@ export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
 export SSH_USER="${SSH_USER:=rocky}"
+export REGION="${REGION:=us-east-1}"
+export ZONE0="${ZONE0:=us-east-1a}"
+export ZONE1="${ZONE1:=us-east-1b}"
+export ZONE2="${ZONE2:=us-east-1c}"
+# Benchmarker instance
+export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5.9xlarge}"
+# Postgres Instance
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.12xlarge}" # Assumes attached storage devices from instance type
 
 # TPCC
 export TPCC_DURATION="${TPCC_DURATION:=20}"
