84b94ad054471dacf65cc73f76580c27ccdbc002
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 22:41:35 +0000

    aws-pgbench-postgresql - expose image template variables for database instance and move inventory to terraform templating

--- aws-pgbench-postgresql/environment.sh
+++ aws-pgbench-postgresql/environment.sh
@@ -7,11 +7,13 @@ export BENCHMARK_NAME="${BENCHMARK_NAME:=AWS_PGBENCH_POSTGRESQL}"
 export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
-export SSH_USER="${SSH_USER:=rocky}"
 export REGION="${REGION:=us-east-1}"
 export ZONE0="${ZONE0:=us-east-1b}"
 # Postgres Instance
 export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.18xlarge}" # Assumes attached storage devices from instance type
+export PG_IMAGE_NAME="${PG_IMAGE_NAME:=Rocky-8-ec2-8.6-20220515.0.x86_64}"
+export PG_IMAGE_OWNER="${PG_IMAGE_OWNER:=679593333241}"
+export PG_SSH_USER="${PG_SSH_USER:=rocky}"
 
 # PGBench
 export PG_VERSIONS="${PG_VERSIONS:=['15.3','14.8','13.11','12.15','11.20','10.23','9.6.24']}"
