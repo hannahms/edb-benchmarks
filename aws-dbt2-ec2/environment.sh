f913e74dad411a8fec475bf652863f41f5d1e942
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 15:56:46 +0000

    aws-dbt2-ec2 - move infrastructure file to a jinja template and create final file during provision step

--- aws-dbt2-ec2/environment.sh
+++ aws-dbt2-ec2/environment.sh
@@ -8,6 +8,15 @@ export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
 export SSH_USER="${SSH_USER:=rocky}"
+export REGION="${REGION:=us-east-1}"
+export ZONE0="${ZONE0:=us-east-1b}"
+# Benchmarker instance
+export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5.9xlarge}"
+# Postgres Instance
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=r5.8xlarge}"
+export PG_STORAGE_TYPE="${PG_STORAGE_TYPE:=io2}"
+export PG_STORAGE_SIZE="${PG_STORAGE_SIZE:=4096}"
+export PG_STORAGE_IOPS="${PG_STORAGE_IOPS:=64000}"
 
 # DBT2
 export DBT2_DURATION="${DBT2_DURATION:=10000}"
