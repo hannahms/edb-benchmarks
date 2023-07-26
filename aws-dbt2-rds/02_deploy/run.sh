21c921f3ebde15772113276a0308b203718cd658
Rachel DiMaggio <rdimaggio2010@gmail.com>
2023-07-26 15:49:08 -0400

    Update syntax and addressed feedback

--- aws-dbt2-rds/02_deploy/run.sh
+++ aws-dbt2-rds/02_deploy/run.sh
@@ -10,5 +10,6 @@ export ANSIBLE_HOST_KEY_CHECKING=false
 
 ansible-playbook \
 	-i "${TERRAFORM_PROJECT_PATH}/../inventory.yml" \
+	-e "@${SOURCEDIR}/../environment.yml" \
 	-e "@${SOURCEDIR}/../vars.yml" \
 	"${SOURCEDIR}/playbook-deploy.yml"
--- aws-tpcc-tde/05_push_results/run.sh
+++ aws-tpcc-tde/05_push_results/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eux
-# Push DBT2 files to the S3 bucket
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # Upload benchmark data
-RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 aws s3 cp "$RESULTS_DIRECTORY/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/" --recursive
