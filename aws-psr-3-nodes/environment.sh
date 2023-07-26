478189ea207c7af8e28808cdb8fd92adef77dd5a
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 22:46:30 +0000

    aws-psr-3-nodes - expose image template variables for database instance and move inventory to terraform templating

--- aws-psr-3-nodes/environment.sh
+++ aws-psr-3-nodes/environment.sh
@@ -11,7 +11,6 @@ export REPO_PASSWORD="${REPO_PASSWORD:=<secret>}"
 export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
-export SSH_USER="${SSH_USER:=rocky}"
 export REGION="${REGION:=us-east-2}"
 export ZONE0="${ZONE0:=us-east-2a}"
 export ZONE1="${ZONE1:=us-east-2b}"
@@ -20,6 +19,9 @@ export ZONE2="${ZONE2:=us-east-2c}"
 export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5.9xlarge}"
 # Postgres Instance
 export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.12xlarge}" # Assumes attached storage devices from instance type
+export PG_IMAGE_NAME="${PG_IMAGE_NAME:=Rocky-8-ec2-8.6-20220515.0.x86_64}"
+export PG_IMAGE_OWNER="${PG_IMAGE_OWNER:=679593333241}"
+export PG_SSH_USER="${PG_SSH_USER:=rocky}"
 
 # TPCC
 export TPCC_DURATION="${TPCC_DURATION:=20}"
