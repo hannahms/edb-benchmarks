49f1771c2fcc29427ce5b2402f14814f77ac570b
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 03:51:01 +0000

    aws-lsr-3-nodes - generate environment.yml based on environment.sh and use it

--- aws-lsr-3-nodes/03_prepare/run.sh
+++ aws-lsr-3-nodes/03_prepare/run.sh
@@ -10,10 +10,9 @@ export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
 
 ansible-playbook \
-	-u ${SSH_USER} \
-	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SOURCEDIR}/../inventory.yml \
-	-e "@${SOURCEDIR}/../vars.yml" \
-	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
-	-e "tpcc_loader_vusers=${TPCC_LOADER_VUSERS}" \
-	${SOURCEDIR}/playbook-tpcc-build-db.yml
+    -u ${SSH_USER} \
+    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+    -i ${SOURCEDIR}/../inventory.yml \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+    "${SOURCEDIR}/playbook-tpcc-build-db.yml"
