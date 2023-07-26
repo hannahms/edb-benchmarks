8264ea1a625bbc9d71b8e9f920327f7780f8668e
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 22:52:20 +0000

    aws-tpcc-tde-medium-amd - expose image template variables for database instance and move inventory to terraform templating

--- aws-tpcc-tde-medium-amd/02_deploy/run.sh
+++ aws-tpcc-tde-medium-amd/02_deploy/run.sh
@@ -9,21 +9,14 @@ export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
 
-python3 ${SOURCEDIR}/build-inventory.py ${TERRAFORM_PROJECT_PATH}
-mv ${SOURCEDIR}/inventory.yml ${SOURCEDIR}/../.
-
 ansible-playbook \
-    -u ${SSH_USER} \
-    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-    -i "${SOURCEDIR}/../inventory.yml" \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
     -e "@$SOURCEDIR/../environment.yml" \
     -e "@$SOURCEDIR/../vars.yml" \
     "${SOURCEDIR}/playbook-deploy.yml"
 
 ansible-playbook \
-    -u ${SSH_USER} \
-    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-    -i "${SOURCEDIR}/../inventory.yml" \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
     -e "@$SOURCEDIR/../environment.yml" \
     -e "@$SOURCEDIR/../vars.yml" \
     "${SOURCEDIR}/playbook-hammerdb-setup.yml"
