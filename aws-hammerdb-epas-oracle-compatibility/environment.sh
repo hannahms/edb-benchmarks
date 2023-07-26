45072efeeae23830382db50a1706f1b859df5b92
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 16:14:48 +0000

    aws-hammerdb-epas-oracle-compatibility - move infrastructure file to a jinja template and create final file during provision step

--- aws-hammerdb-epas-oracle-compatibility/environment.sh
+++ aws-hammerdb-epas-oracle-compatibility/environment.sh
@@ -12,6 +12,15 @@ export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
 export SSH_USER="${SSH_USER:=rocky}"
+export REGION="${REGION:=us-east-1}"
+export ZONE0="${ZONE0:=us-east-1b}"
+# Benchmarker instance
+export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5.4xlarge}"
+# Postgres Instance
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5.4xlarge}"
+export PG_STORAGE_TYPE="${PG_STORAGE_TYPE:=io2}"
+export PG_STORAGE_SIZE="${PG_STORAGE_SIZE:=1500}"
+export PG_STORAGE_IOPS="${PG_STORAGE_IOPS:=32000}"
 
 # TPROCC
 export TPROCC_DURATION="${TPROCC_DURATION:=60}"
