182f957f062e4f41644fd563d51e1bfaac13681d
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 22:37:01 +0000

    aws-hammerdb-tproc-c-oracle - move inventory to terraform templating

--- aws-hammerdb-tproc-c-oracle/02_deploy/run.sh
+++ aws-hammerdb-tproc-c-oracle/02_deploy/run.sh
@@ -9,13 +9,8 @@ export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
 
-python3 ./build-inventory.py "${TERRAFORM_PROJECT_PATH}"
-mv inventory.yml ../.
-
 ansible-playbook \
-    -u ${SSH_USER} \
-    --private-key "${TERRAFORM_PROJECT_PATH}/ssh-id_rsa" \
-    -i "$SOURCEDIR/../inventory.yml" \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
     -e "@$SOURCEDIR/../environment.yml" \
     -e "@$SOURCEDIR/../vars.yml" \
     -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
