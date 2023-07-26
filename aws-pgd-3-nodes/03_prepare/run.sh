057829ce04253bd871a48e4066457420acfa8c5b
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 04:17:41 +0000

    aws-pgd-3-nodes - generate environment.yml based on environment.sh and use it

---
+++ aws-pgd-3-nodes/03_prepare/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+ansible-playbook \
+    -u ${SSH_USER} \
+    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+    -i "${SOURCEDIR}/../inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+    "${SOURCEDIR}/playbook-tpcc-build-db.yml"
+
+ansible-playbook \
+    -u ${SSH_USER} \
+    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+    -i "${SOURCEDIR}/../inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+    "${SOURCEDIR}/playbook-post-build-db.yml"
---
+++ aws-pgd-3-nodes/04_execute/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+# Run the ramping up benchmark
+ansible-playbook \
+    -u ${SSH_USER} \
+    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+    -i "${SOURCEDIR}/../inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+    "${SOURCEDIR}/playbook-setup-sync-repl.yml"
+
+ansible-playbook \
+    -u ${SSH_USER} \
+    --private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+    -i "${SOURCEDIR}/../inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+    -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+    -e "results_directory=${RESULTS_DIRECTORY}/report-data" \
+    "${SOURCEDIR}/playbook-tpcc-run-rampup.yml"
+
+# Copy infrastructure.yml and vars.yml
+cp "../infrastructure.yml" "$RESULTS_DIRECTORY"
+cp "../vars.yml" "$RESULTS_DIRECTORY"
