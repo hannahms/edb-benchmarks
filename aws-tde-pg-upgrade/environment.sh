1130cc04efcd583bd060325363c0d7953c31bc11
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-26 17:12:11 +0000

    aws-tde-pg-upgrade - move infrastructure file to a jinja template and create final file during provision step

--- aws-tde-pg-upgrade/environment.sh
+++ aws-tde-pg-upgrade/environment.sh
@@ -4,14 +4,18 @@ export BUCKET_NAME="${BUCKET_NAME:=ebac-reports}"
 export BENCHMARK_NAME="${BENCHMARK_NAME:=AWS_TDE_PG_UPGRADE}"
 
 # Credentials
-export EDB_REPO_USERNAME="${EDB_REPO_USERNAME:=<secret>}"
-export EDB_REPO_PASSWORD="${EDB_REPO_PASSWORD:=<secret>}"
+export REPO_USERNAME="${REPO_USERNAME:=<secret>}"
+export REPO_PASSWORD="${REPO_PASSWORD:=<secret>}"
 
 # Ansible
 export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Terraform
 export SSH_USER="${SSH_USER:=rocky}"
+export REGION="${REGION:=us-east-1}"
+export ZONE0="${ZONE0:=us-east-1b}"
+# Postgres Instance
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.12xlarge}" # Assumes attached storage devices from instance type
 
 # TPCC
 export TPCC_WAREHOUSE="${TPCC_WAREHOUSE:=2000}"
