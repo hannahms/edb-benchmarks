39c2d6a454f4a359c9fce5900c040ad417cf2882
Mark Wong <mark.wong@enterprisedb.com>
2023-08-14 09:27:12 -0700

    aws-lsr-3-nodes: update defaults
    Use c5d.18xlarge for postgres nodes and use 1000 for the TPROC-C scale
    factor.

--- aws-lsr-3-nodes/environment.sh
+++ aws-lsr-3-nodes/environment.sh
@@ -3,6 +3,10 @@
 export BUCKET_NAME="${BUCKET_NAME:=ebac-reports}"
 export BENCHMARK_NAME="${BENCHMARK_NAME:=AWS_LSR_3_NODES}"
 
+# Credentials
+export REPO_USERNAME="${REPO_USERNAME:=<secret>}"
+export REPO_PASSWORD="${REPO_PASSWORD:=<secret>}"
+
 # Ansible
 export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
@@ -14,14 +18,14 @@ export ZONE2="${ZONE2:=us-east-1c}"
 # Benchmarker instance
 export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5.9xlarge}"
 # Postgres Instance
-export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.12xlarge}" # Assumes attached storage devices from instance type
+export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.18xlarge}" # Assumes attached storage devices from instance type
 export PG_IMAGE_NAME="${PG_IMAGE_NAME:=Rocky-8-ec2-8.6-20220515.0.x86_64}"
 export PG_IMAGE_OWNER="${PG_IMAGE_OWNER:=679593333241}"
 export PG_SSH_USER="${PG_SSH_USER:=rocky}"
 
 # TPCC
 export TPCC_DURATION="${TPCC_DURATION:=20}"
-export TPCC_WAREHOUSE="${TPCC_WAREHOUSE:=2000}"
+export TPCC_WAREHOUSE="${TPCC_WAREHOUSE:=1000}"
 export TPCC_RAMPUP="${TPCC_RAMPUP:=1}"
 export TPCC_LOADER_VUSERS="${TPCC_LOADER_VUSERS:=75}"
 export TPCC_MIN_VUSERS="${TPCC_MIN_VUSERS:=5}"
