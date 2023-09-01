69fffe921389799a8e772426be30bc59854a966b
Mark Wong <mark.wong@enterprisedb.com>
2023-09-01 13:54:14 -0700

    aws-pgd-3-nodes: capture system logs
    Also enable crash dump collection.

--- aws-pgd-3-nodes/04_execute/run.sh
+++ aws-pgd-3-nodes/04_execute/run.sh
@@ -9,6 +9,7 @@ export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
+export ANSIBLE_ROLES_PATH="${SOURCEDIR}/../../roles"
 
 ansible-playbook \
     -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
@@ -31,3 +32,10 @@ for CONNECTIONS in $(seq "${DBT2_CONNECTIONS_MIN}" "${DBT2_CONNECTIONS_STEP}" \
 		-e "ts_dbstat=/tmp/dbstat-${CONNECTIONS}" \
 		"${SOURCEDIR}/playbook-execute.yml"
 done
+
+ansible-playbook \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../vars.yml" \
+	-e "results_directory=${RESULTS_DIRECTORY}" \
+    "${SOURCEDIR}/playbook-save.yml"
