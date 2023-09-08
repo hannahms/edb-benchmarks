1cb62015b65b9aa3530c0b74c78da1d2d9ee6661
bryan <31219516+bryan-bar@users.noreply.github.com>
2023-09-08 10:57:21 -0700

    Revert "Update each benchmarks validation step to use benchmark-steps role with validate task"
    This reverts commit 87b06cf92502389fdc89a5e6e9f4fbfbba15cb77.

--- aws-dbt2-aurora/00_validate/run.sh
+++ aws-dbt2-aurora/00_validate/run.sh
@@ -1,30 +1,14 @@
 #!/bin/bash -eux
 
-#!/bin/bash -eux
-
 SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
-ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
 TERRAFORM_PROJECT_NAME="terraform"
-TERRAFORM_PROJECT_PATH="${BENCHMARK_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
 TERRAFORM_PLAN_FILENAME="terraform.plan"
-RESULTS_DIRECTORY="${BENCHMARK_DIRECTORY}/results"
-ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
-# stringified json or a json/yaml files full path
-OVERRIDES="${OVERRIDES:=}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
 
-ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
-    ansible-playbook "${SOURCEDIR}/run.yml" \
-        -e "env_yml_file=${BENCHMARK_DIRECTORY}/environment.yml" \
-        -e "env_source_file=${BENCHMARK_DIRECTORY}/environment.sh" \
-        -e "vars_file=${BENCHMARK_DIRECTORY}/vars.yml" \
-        -e "overrides=${OVERRIDES}" \
-        -e "root_directory=${ROOT_DIRECTORY}" \
-        -e "benchmark_directory=${BENCHMARK_DIRECTORY}" \
-        -e "results_directory=${RESULTS_DIRECTORY}" \
-        -e "terraform_project_name=${TERRAFORM_PROJECT_NAME}" \
-        -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
-        -e "terraform_plan_filename=${TERRAFORM_PLAN_FILENAME}"
+ansible-playbook "${SOURCEDIR}/run.yml" \
+    -e "vars_file=${SOURCEDIR}/../environment.yml" \
+    -e "env_file=${SOURCEDIR}/../environment.sh"
 
 echo "DBT2_DURATION: $DBT2_DURATION"
 echo "DBT2_WAREHOUSE: $DBT2_WAREHOUSE"
