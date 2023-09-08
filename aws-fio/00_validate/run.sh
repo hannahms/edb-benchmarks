bb358badcfd237b2bf24ac2abd0d0ee8682d609f
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-09-08 15:51:39 +0000

    Update each benchmarks validation step to use benchmark-steps role with validate task

--- aws-fio/00_validate/run.sh
+++ aws-fio/00_validate/run.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -eux
 
 SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-ansible-playbook "${SOURCEDIR}/run.yml" \
-    -e "vars_file=${SOURCEDIR}/../environment.yml" \
-    -e "env_file=${SOURCEDIR}/../environment.sh"
+BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
+ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${BENCHMARK_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
+TERRAFORM_PLAN_FILENAME="terraform.plan"
+RESULTS_DIRECTORY="${BENCHMARK_DIRECTORY}/results"
+ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
+# stringified json or a json/yaml files full path
+OVERRIDES="${OVERRIDES:=}"
+
+ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
+    ansible-playbook "${SOURCEDIR}/run.yml" \
+        -e "env_yml_file=${BENCHMARK_DIRECTORY}/environment.yml" \
+        -e "env_source_file=${BENCHMARK_DIRECTORY}/environment.sh" \
+        -e "vars_file=${BENCHMARK_DIRECTORY}/vars.yml" \
+        -e "interpolate_file=${BENCHMARK_DIRECTORY}/interpolate.yml" \
+        -e "overrides=${OVERRIDES}" \
+        -e "root_directory=${ROOT_DIRECTORY}" \
+        -e "benchmark_directory=${BENCHMARK_DIRECTORY}" \
+        -e "results_directory=${RESULTS_DIRECTORY}" \
+        -e "terraform_project_name=${TERRAFORM_PROJECT_NAME}" \
+        -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+        -e "terraform_plan_filename=${TERRAFORM_PLAN_FILENAME}"
