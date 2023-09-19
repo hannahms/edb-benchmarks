64c923135e60de15dc1d058af3c3a12821b700d9
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-09-19 21:19:03 +0000

    Remove validations within bash script

--- aws-pgbench-postgresql/00_validate/run.sh
+++ aws-pgbench-postgresql/00_validate/run.sh
@@ -9,11 +9,3 @@ ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
     ansible-playbook "${SOURCEDIR}/run.yml" \
         -e "root_directory=${ROOT_DIRECTORY}" \
         -e "benchmark_directory=${BENCHMARK_DIRECTORY}"
-
-echo "PGBENCH_SCALE_FACTOR: $PGBENCH_SCALE_FACTOR"
-
-if (( $(echo "$PGBENCH_SCALE_FACTOR < 100" | bc -l) )); then
-   echo "PGBENCH_SCALE_FACTOR: $PGBENCH_SCALE_FACTOR";
-   echo "PGBench Scale Factor, cannot be lower than 100!"
-   exit 1
-fi
