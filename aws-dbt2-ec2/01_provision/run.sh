c3a45e0dcdcd67456e48d8a5a3c2908bb924fa4f
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-09-19 20:35:24 +0000

    Use provision task changes

--- aws-dbt2-ec2/01_provision/run.sh
+++ aws-dbt2-ec2/01_provision/run.sh
@@ -1,40 +1,13 @@
 #!/bin/bash -eux
-
 SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-TERRAFORM_PROJECT_NAME="terraform"
-TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
-TERRAFORM_PLAN_FILENAME="terraform.plan"
-RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
+ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
+ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
 
-ansible-playbook "$SOURCEDIR/generate.yml" \
-    -e "template_file=$SOURCEDIR/templates/infrastructure.yml.j2" \
-    -e "dest_file=$SOURCEDIR/../infrastructure.yml" \
-    -e "@$SOURCEDIR/../environment.yml" \
-    -e "@$SOURCEDIR/../credentials.yml"
-
-edb-terraform generate \
-		--cloud-service-provider aws \
-		--project-name "${TERRAFORM_PROJECT_NAME}" \
-		--work-path "${SOURCEDIR}/../" \
-		--user-templates "${SOURCEDIR}/templates" \
-		--infra-file "${SOURCEDIR}/../infrastructure.yml"
-cd "${TERRAFORM_PROJECT_PATH}"
-terraform init
-
-# Save terraform plan for inspection/reuse
-terraform plan -out="$TERRAFORM_PLAN_FILENAME"
-
-# terraform.tfstate/.tfstate.backup will be left around since we use short-term credentials
-terraform apply -auto-approve "$TERRAFORM_PLAN_FILENAME"
-
-mkdir -p "${RESULTS_DIRECTORY}"
-# .tfstate might contain secrets
-# ssh short term keys currently used
-# .terraform created at run-time and controlled by terraform CLI
-rsync --archive \
-		--exclude="*tfstate*" \
-		--exclude="*ssh*" \
-		--exclude=".terraform/" \
-		--recursive \
-		"${TERRAFORM_PROJECT_PATH}" \
-		"${RESULTS_DIRECTORY}"
+ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
+    ansible-playbook "${SOURCEDIR}/generate.yml" \
+        -e "template_file=$SOURCEDIR/templates/infrastructure.yml.j2" \
+        -e "dest_file=$SOURCEDIR/../infrastructure.yml" \
+        -e "user_templates=$SOURCEDIR/templates" \
+        -e "@$SOURCEDIR/../environment.yml" \
+        -e "@$SOURCEDIR/../credentials.yml"
