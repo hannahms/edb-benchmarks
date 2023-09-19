266fc70634437aa5236797b2978785ea031a5137
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-09-19 21:46:42 +0000

    Use unprovision task changes

--- aws-dbt2-aurora/06_unprovision/run.sh
+++ aws-dbt2-aurora/06_unprovision/run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -eux
-
 SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-TERRAFORM_PROJECT_NAME="terraform"
-TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
-cd "${TERRAFORM_PROJECT_PATH}"
-terraform destroy -auto-approve
+BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
+ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
+ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
+
+ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
+    ansible-playbook \
+        -e "@$BENCHMARK_DIRECTORY/environment.yml" \
+        -e "@$BENCHMARK_DIRECTORY/credentials.yml" \
+        /dev/stdin <<'EOF'
+---
+- hosts: localhost
+  name: Destroy resources
+  gather_facts: false
+
+  tasks:
+    - name: Role - benchmark-steps | Task - 06_unprovision.yml
+      ansible.builtin.include_role:
+        name: benchmark-steps
+        tasks_from: 06_unprovision.yml
+EOF
