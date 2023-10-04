14b4b9e6810eb8f7c9232b4a9b0ce36d5b8ed33f
alessandroferraresi <alessandro.ferraresi@enterprisedb.com>
2023-10-04 11:54:58 +0200

    Added ARM Benchmark on AWS Graviton EC2

---
+++ aws-dbt2-ec2-arm/04_execute/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+# Run the benchmark
+# FIXME: use absolute path to vars.yml
+ansible-playbook \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../credentials.yml" \
+    -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+    -e "results_directory=${RESULTS_DIRECTORY}" \
+    "${SOURCEDIR}/playbook-dbt2-run.yml"
+
+tar -C "${RESULTS_DIRECTORY}" --strip-components=1 \
+		-xf "${RESULTS_DIRECTORY}/dbt2-data.tar.gz"
+rm "${RESULTS_DIRECTORY}/dbt2-data.tar.gz"
