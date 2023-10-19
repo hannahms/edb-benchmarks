392ea9847a5d852921720160a6325fac1ec36eeb
hannahms <36494235+hannahms@users.noreply.github.com>
2023-10-19 18:31:23 -0400

    camo ready to go - init for gc

---
+++ aws-pgbench-pgd-camo/05_push_results/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
+ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
+ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
+TERRAFORM_PLAN_FILENAME="terraform.plan"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
+ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
+    ansible-playbook \
+        -e "@$SOURCEDIR/../environment.yml" \
+        -e "@$SOURCEDIR/../credentials.yml" \
+        -e "results_directory=${RESULTS_DIRECTORY}" \
+        "${SOURCEDIR}/run.yml"
