6dd113b74cf57e316ab0924dd5728c530b2695ac
alessandroferraresi <alessandro.ferraresi@enterprisedb.com>
2023-10-24 17:29:56 +0200

    Added BA scaling tests on AWS

---
+++ aws-hammerdb-tproc-c-biganimal-scaling/00_validate/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+BENCHMARK_DIRECTORY="$(realpath "${SOURCEDIR}/..")"
+ROOT_DIRECTORY="$(realpath "${BENCHMARK_DIRECTORY}/..")"
+ANSIBLE_ROLES_PATH="${ROOT_DIRECTORY}/roles"
+
+ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH \
+    ansible-playbook "${SOURCEDIR}/run.yml" \
+        -e "root_directory=${ROOT_DIRECTORY}" \
+        -e "benchmark_directory=${BENCHMARK_DIRECTORY}"
---
+++ aws-hammerdb-tproc-c-biganimal-scaling/04_execute/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+SOURCEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+TERRAFORM_PROJECT_NAME="terraform"
+TERRAFORM_PROJECT_PATH="${SOURCEDIR}/../${TERRAFORM_PROJECT_NAME}"
+RESULTS_DIRECTORY="${SOURCEDIR}/../results"
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+# Run the benchmark
+ansible-playbook \
+    -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
+    -e "@$SOURCEDIR/../environment.yml" \
+    -e "@$SOURCEDIR/../credentials.yml" \
+    -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+    -e "results_directory=${RESULTS_DIRECTORY}/tprocc-data" \
+    ./playbook-tprocc-run.yml
+
+# Copy infrastructure.yml and vars.yml
+cp "../infrastructure.yml" "$RESULTS_DIRECTORY/tprocc-data"
+cp "../credentials.yml" "$RESULTS_DIRECTORY/tprocc-data"
